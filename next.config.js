59c35e69d5380010e4d60b844bf45b61a84cb7b2
ToanVQ <toan.vuquoc@toshiba-tsdv.com>
2019-07-29 10:53:03 +0900

    optimize webpack for moment

--- next.config.js
+++ next.config.js
@@ -4,11 +4,16 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer(
-  withSass({
+module.exports = withBundleAnalyzer({
+  ...withSass({
     ...withCss({
       cssModules: true,
     }),
     cssModules: true,
-  })
-);
+  }),
+  webpack(config, { webpack }) {
+    // Ignore all locale files of moment.js
+    config.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
+    return config;
+  },
+});
