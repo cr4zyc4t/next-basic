46b7f1347f734e780d0fcbf50aad0c85aafa0c6a
ToanVQ <toan.vuquoc@toshiba-tsdv.com>
2019-07-29 10:44:57 +0900

    add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const withCss = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
-
-module.exports = withSass({
-  ...withCss({
-    cssModules: true,
-  }),
-  cssModules: true,
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
+
+module.exports = withBundleAnalyzer(
+  withSass({
+    ...withCss({
+      cssModules: true,
+    }),
+    cssModules: true,
+  })
+);
